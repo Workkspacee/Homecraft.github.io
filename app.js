d54efec507b243ba0cfbab02a48215e09c4a0707
Anushi72 <anushipumbhadiya@gmail.com>
2024-04-02 12:24:54 +0530

    commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 // view engine dynamic data use karva ma help kare 
 app.set('view engine','ejs');
 
+//for linking static file(css files)
+app.use(express.static('css'));
+app.use(express.static('Script'));
+
 app.listen(9362);
 
 app.get('/',(req, res) => {
