faebfcb50da337ade8584fbe3f3e171203257e7b
Anushi72 <anushipumbhadiya@gmail.com>
2024-04-02 11:49:36 +0530

    commit

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 
 const app = express();
 
+// view engine dynamic data use karva ma help kare 
+app.set('view engine','ejs');
+
 app.listen(9362);
 
 app.get('/',(req, res) => {
-    res.sendFile('./views/login.html',{ root:__dirname});
+    res.render('login');
 });
