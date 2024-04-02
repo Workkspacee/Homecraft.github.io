5158ff0eca8142ba243810879de9535766c60691
Anushi72 <anushipumbhadiya@gmail.com>
2024-04-02 13:49:30 +0530

    commit

--- app.js
+++ app.js
@@ -11,22 +11,12 @@ app.use(express.static('Script'));
 
 app.listen(9362);
 
-app.get('/',(req, res) => {
-    res.render('login');
-});
+console.log('connection established');
 
-app.get('/backend',(req, res) => {
-    res.render('backend');
-});
-
-app.get('/backend2',(req, res) => {
-    res.render('backend2');
-});
-
-app.get('/fiter',(req, res) => {
-    res.render('fiter');
-});
-
-app.get('/tailor',(req, res) => {
-    res.render('tailor');
-});
+app.get('/', (req, res) => res.render('login'));
+app.get('/backend', (req, res) => res.render('backend'));
+app.get('/backend2', (req, res) => res.render('backend2'));
+app.get('/fiter', (req, res) => res.render('fiter'));
+app.get('/fiter2', (req, res) => res.render('fiter2'));
+app.get('/tailor', (req, res) => res.render('tailor'));
+app.get('/admin', (req, res) => res.render('admin'));
