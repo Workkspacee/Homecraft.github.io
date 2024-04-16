b7a9348f7ac5283d1b3630fb2c892cd5fea75336
Anushi72 <anushipumbhadiya@gmail.com>
2024-04-16 12:16:29 +0530

    commit

---
+++ models/data.js
@@ -0,0 +1,15 @@
+const { text } = require('express');
+const mongoose = require('mongoose');
+
+const dataSchema = new mongoose.Schema({
+    work_no: { type: String, required: true, unique: true },
+    date: { type: Date},
+    d_date: { type: Date},
+    p_no: { type: Number, required: true},
+    add: { type: String},
+    f_type: { type: String},
+    f_status: { type: String, enum: ['order pending','delivery pending', 'delivered'], default: 'pending'},
+    w_status: { type: String, enum: ['details pending', 'measurement pending', 'given for stiching', 'delivery pending', 'order delivered'], default: 'pending'},
+});
+
+module.exports = mongoose.model('data', dataSchema);
