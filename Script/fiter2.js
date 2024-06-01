e74a57d45d1788304248e693238d4fbf72fae818
Anushi72 <anushipumbhadiya@gmail.com>
2024-06-01 10:22:52 +0530

    c

--- Script/fiter2.js
+++ Script/fiter2.js
@@ -171,30 +171,6 @@ var counter = 1; // Counter for unique labels and names
       ///////////////////////////////////////
 
 
-      // Create a new text field
-      var textField = document.createElement("input");
-      textField.setAttribute("type", "text");
-      textField.classList.add('login__add__text');
-  
-      // Create a unique label and name
-      var label = document.createElement("label");
-      label.innerHTML = "Fabric req (meter) " + " : ";
-      var name = "fabric_req" + counter;
-  
-      // Set name and id attribute for the text field
-      textField.setAttribute("name", name);
-      textField.setAttribute("id", name);
-  
-      // Append the label and text field to the form
-      var form = document.getElementById("myForm");
-      form.appendChild(label);
-      form.appendChild(textField);
-      form.appendChild(document.createElement("br"));
-    
-
-      ///////////////////////////////////
-
-
        // Create a new text field
        var textField = document.createElement("input");
        textField.setAttribute("type", "text");
@@ -213,34 +189,15 @@ var counter = 1; // Counter for unique labels and names
        var form = document.getElementById("myForm");
        form.appendChild(label);
        form.appendChild(textField);
+       form.appendChild(document.createElement("br"));
+       form.appendChild(document.createElement("br"));
+       form.appendChild(document.createElement("br"));
 
 
        ///////////////////////////////////////
 
 
-        // Create a new text field
-      var textField = document.createElement("input");
-      textField.setAttribute("type", "text");
-      textField.classList.add('login__add__text');
-  
-      // Create a unique label and name
-      var label = document.createElement("label");
-      label.innerHTML = "Blackout req (meter) " + " : ";
-      var name = "blackout_req" + counter;
-  
-      // Set name and id attribute for the text field
-      textField.setAttribute("name", name);
-      textField.setAttribute("id", name);
-  
-      // Append the label and text field to the form
-      var form = document.getElementById("myForm");
-      form.appendChild(label);
-      form.appendChild(textField);
-      form.appendChild(document.createElement("br"));
-      form.appendChild(document.createElement("br"));
-      form.appendChild(document.createElement("br"));
- 
-
+      
     // Increment the counter for the next label and name
     counter++;
   }
